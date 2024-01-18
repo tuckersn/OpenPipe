8908911e351419e7f5946bddee4045563499f674
Kyle Corbitt <kyle@corbt.com>
2024-01-17 19:15:58 -0800

    anyscale testing

--- infra/src/app-env.ts
+++ infra/src/app-env.ts
@@ -32,6 +32,7 @@ export const environment = new kubernetes.core.v1.Secret(
       AZURE_OPENAI_API_KEY_JAPANEAST: getSecret("AZURE_OPENAI_API_KEY_JAPANEAST"),
       AZURE_OPENAI_API_KEY_NORWAYEAST: getSecret("AZURE_OPENAI_API_KEY_NORWAYEAST"),
       AZURE_OPENAI_API_KEY_SOUTHINDIA: getSecret("AZURE_OPENAI_API_KEY_SOUTHINDIA"),
+      ANYSCALE_INFERENCE_API_KEY: getSecret("ANYSCALE_INFERENCE_API_KEY"),
 
       WORKER_CONCURRENCY: getConfig("WORKER_CONCURRENCY"),
       PG_MAX_POOL_SIZE: getConfig("PG_MAX_POOL_SIZE"),
@@ -44,6 +45,7 @@ export const environment = new kubernetes.core.v1.Secret(
       MODAL_ENVIRONMENT: getConfig("MODAL_ENVIRONMENT"),
       SMTP_HOST: getConfig("SMTP_HOST"),
       SENDER_EMAIL: getConfig("SENDER_EMAIL"),
+      ANYSCALE_INFERENCE_BASE_URL: getConfig("ANYSCALE_INFERENCE_BASE_URL"),
       NEXT_PUBLIC_HOST: appUrl,
       NEXTAUTH_URL: appUrl,
     },
